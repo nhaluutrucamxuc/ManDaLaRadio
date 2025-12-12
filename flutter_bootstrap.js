21eaa16749027a3ef3641dc6417da01e3957c5b6
nhaluutrucamxuc <nhaluutrucamxuc@gmail.com>
2025-12-12 23:51:34 +0700

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1808188755"
+    serviceWorkerVersion: "1827981672"
   }
 });
